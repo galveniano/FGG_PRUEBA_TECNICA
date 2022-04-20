5cf5e91e5529d47a654628b2a137b673d7b10833
fgrinan <fgrinan@trust@portal.org>
2022-04-20 17:34:46 +0200

    wip

--- FGG_PRUEBA_TECNICA/Controllers/HomeController.cs
+++ FGG_PRUEBA_TECNICA/Controllers/HomeController.cs
@@ -11,11 +11,10 @@ namespace FGG_PRUEBA_TECNICA.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
         {
-            _logger = logger;
+
         }
 
         public IActionResult Index()
